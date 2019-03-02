0cdabf44eb43007e8b0a9b9ab2f3f280430ecc6e
Tomáš Javůrek <xvjavurek@vutbr.cz>
2019-03-02 20:46:03 +0100

    change: nextSlide, prevSlide logic + refactor

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -20,10 +20,11 @@ export class ImageSliderComponent implements OnInit {
 
   // inputs
   @Input() title: string;
-  @Input() images: Array<any>;
+  @Input() images = [];
+
 
   // outputs
-  @Output() activeSlide: EventEmitter<any> = new EventEmitter<any>();
+  // @Output() activeSlide: EventEmitter<any> = new EventEmitter<any>();
 
   // slider ElementRef
   @ViewChild('slider') slider: ElementRef;
@@ -31,15 +32,19 @@ export class ImageSliderComponent implements OnInit {
   // public vars
   imageIndex = 0;
   numberOfImages: number;
+  sliderContainer: HTMLElement;
 
   constructor(
-    private utils: UtilsService
+    private utils: UtilsService,
+    private elm: ElementRef
   ) {}
 
   ngOnInit() {
     // console.log('slider', this.slider);
     // set numberOfImages
     this.numberOfImages = this.images.length;
+    this.sliderContainer = this.elm.nativeElement.querySelector('.slider-container');
+    this.setSliderWidth();
   }
 
   getIdFromName(name: string): string {
@@ -65,45 +70,41 @@ export class ImageSliderComponent implements OnInit {
     lazyImage.src = image.dataset.src;
     lazyImage.srcset = image.dataset.srcset;
     // change state for class
-    imageContainer.nativeElement.classList.remove('inactive');
-    imageContainer.nativeElement.classList.add('active');
+    /* imageContainer.nativeElement.classList.remove('inactive');
+    imageContainer.nativeElement.classList.add('active'); */
   }
 
   nextImage() {
-    // current slide is prevSlide
-    const prevSlide = this.images[this.imageIndex].parentElement;
-    // change index of active slide
-    this.imageIndex = (
-      (this.imageIndex < this.numberOfImages - 1) ?
-      this.imageIndex + 1 :
-      0
-    );
-    // emit event
-    this.activeSlide.emit({
-      slider: this.slider.nativeElement,
-      activeSlideIndex: this.imageIndex,
-      previousSlideElement: prevSlide,
-      activeSlideElement: this.images[this.imageIndex].parentElement,
-      slideDirection: 'next'
-    });
+    console.log('------------> nextImage');
+
+    const sliderContainerBox = this.sliderContainer.getBoundingClientRect();
+    const width = sliderContainerBox.width;
+    const slideWidth = width / this.numberOfImages;
+    const left = sliderContainerBox.left - 59;
+    const computedLeft = left - slideWidth;
+    this.sliderContainer.style.left = `${(computedLeft > (-width + slideWidth) ? computedLeft : 0)}px`;
+
   }
   prevImage() {
-    // current slide is prevSlide
-    const prevSlide = this.images[this.imageIndex].parentElement;
-    // change index of active slide
-    this.imageIndex = (
-      (this.imageIndex > 0) ?
-      this.imageIndex - 1 :
-      this.numberOfImages - 1
-    );
-    // emit event
-    this.activeSlide.emit({
-      slider: this.slider.nativeElement,
-      activeSlideIndex: this.imageIndex,
-      previousSlideElement: prevSlide,
-      activeSlideElement: this.images[this.imageIndex].parentElement,
-      slideDirection: 'prev'
-    });
+    console.log('------------> prevImage');
+
+    const sliderContainerBox = this.sliderContainer.getBoundingClientRect();
+    const width = sliderContainerBox.width;
+    const slideWidth = width / this.numberOfImages;
+    const left = sliderContainerBox.left - 59;
+    // console.log('left', left);
+    const computedLeft = left + slideWidth;
+    // console.log('width', width);
+    // console.log('computedLeft', computedLeft);
+    const absoluteLeft = -(width - (2 * slideWidth));
+    // console.log('absoluteLeft', absoluteLeft);
+    // this.sliderContainer.style.left = computedLeft + 'px'; /* `${(computedLeft > 0 ? absoluteLeft : 0)}px`; */
+    this.sliderContainer.style.left = `${(computedLeft > 0 ? absoluteLeft : computedLeft)}px`;
+  }
+
+  private setSliderWidth() {
+    this.sliderContainer.style.width = `calc(((70vw + 400px) * ${this.numberOfImages}) + 30px)`;
+    this.sliderContainer.style.left = '0px';
   }
 
   log(key: any, value: any) {
